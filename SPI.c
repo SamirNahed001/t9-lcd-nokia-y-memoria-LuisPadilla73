05c16c9fd56ac7440461b12204a96409cfc97c31
LuisPadilla73 <135662763+LuisPadilla73@users.noreply.github.com>
2024-04-16 10:11:03 -0600

    Add files via upload

--- SPI.c
+++ SPI.c
@@ -18,71 +18,135 @@
 #include "clock_config.h"
 #include "delay.h"
 
-extern uint8_t g_master_rxBuffImage[][IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_1[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_2[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_3[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_4[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_5[IMAGE_MEMORY_SIZE];
 
 void SPI_config(void)
 {
-    const port_pin_config_t button_config = {kPORT_PullDisable, kPORT_FastSlewRate, kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable, kPORT_LowDriveStrength, kPORT_MuxAlt2, kPORT_UnlockRegister};
-    const uint32_t srcClock_Hz = DSPI_MASTER_CLK_FREQ;
 
-    CLOCK_EnableClock(kCLOCK_Spi0);
-    CLOCK_EnableClock(kCLOCK_PortD);
-    CLOCK_EnableClock(kCLOCK_PortC);
-    CLOCK_EnableClock(kCLOCK_PortB);
+	uint32_t srcClock_Hz;
 
-    for (int i = 0; i < 4; ++i)
-    {
-        PORT_SetPinConfig(PORTD, PIN0_IDX + i, &button_config);
-    }
+	const port_pin_config_t button_config = { //Falta configurar aqui
+			kPORT_PullDisable,
+			kPORT_FastSlewRate,
+	        kPORT_PassiveFilterDisable,
+	        kPORT_OpenDrainDisable,
+	        kPORT_LowDriveStrength,
+	        kPORT_MuxAlt2,
+	 	 	kPORT_UnlockRegister
+	};
 
-    dspi_master_config_t masterConfig;
+	CLOCK_EnableClock(kCLOCK_Spi0);
+	CLOCK_EnableClock(kCLOCK_PortD);
+	CLOCK_EnableClock(kCLOCK_PortC);
+	CLOCK_EnableClock(kCLOCK_PortB);
 
-    /* Master config for LCD */
-    masterConfig.whichCtar = kDSPI_Ctar0;
-    masterConfig.ctarConfig.baudRate = TRANSFER_BAUDRATE;
-    masterConfig.ctarConfig.bitsPerFrame = 8U;
-    masterConfig.ctarConfig.cpol = kDSPI_ClockPolarityActiveHigh;
-    masterConfig.ctarConfig.cpha = kDSPI_ClockPhaseFirstEdge;
-    masterConfig.ctarConfig.direction = kDSPI_MsbFirst;
-    masterConfig.ctarConfig.pcsToSckDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE;
-    masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE;
-    masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE;
-    masterConfig.whichPcs = kDSPI_Pcs0;
-    masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
-    masterConfig.enableContinuousSCK = false;
-    masterConfig.enableRxFifoOverWrite = false;
-    masterConfig.enableModifiedTimingFormat = false;
-    masterConfig.samplePoint = kDSPI_SckToSin0Clock;
+	PORT_SetPinConfig(PORTD, PIN0_IDX, &button_config);
+	PORT_SetPinConfig(PORTD, PIN1_IDX, &button_config);
+	PORT_SetPinConfig(PORTD, PIN2_IDX, &button_config);
+	PORT_SetPinConfig(PORTD, PIN3_IDX, &button_config);
 
-    DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
+	/* Master config */
+	dspi_master_config_t masterConfig;
 
-    /* Master config for Memory */
-    masterConfig.whichCtar = kDSPI_Ctar1;
-    masterConfig.ctarConfig.baudRate = TRANSFER_BAUDRATE_MEMORY;
-    masterConfig.ctarConfig.cpol = kDSPI_ClockPolarityActiveLow;
-    masterConfig.ctarConfig.cpha = kDSPI_ClockPhaseSecondEdge;
-    masterConfig.whichPcs = kDSPI_Pcs1;
+	masterConfig.whichCtar                                = kDSPI_Ctar0;
+	masterConfig.ctarConfig.baudRate                      = TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.bitsPerFrame                  = 8U;
+	masterConfig.ctarConfig.cpol                          = kDSPI_ClockPolarityActiveHigh;
+	masterConfig.ctarConfig.cpha                          = kDSPI_ClockPhaseFirstEdge;
+	masterConfig.ctarConfig.direction                     = kDSPI_MsbFirst;
+	masterConfig.ctarConfig.pcsToSckDelayInNanoSec        = 1000000000U / TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 1000000000U / TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE;
 
-    DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
+	masterConfig.whichPcs           = kDSPI_Pcs0;
+	masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
+
+	masterConfig.enableContinuousSCK        = false;
+	masterConfig.enableRxFifoOverWrite      = false; //Tienes FIFOS; recuibe mas de un byte a la vez, "quiero que me sobrescribas el valor mas alto o no"
+	masterConfig.enableModifiedTimingFormat = false;
+	masterConfig.samplePoint                = kDSPI_SckToSin0Clock;
+
+	srcClock_Hz = DSPI_MASTER_CLK_FREQ;
+	DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
+
+
+	/* For Memory */
+
+	masterConfig.whichCtar                                = kDSPI_Ctar1;
+	masterConfig.ctarConfig.baudRate                      = TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.bitsPerFrame                  = 8U;
+	masterConfig.ctarConfig.cpol                          = kDSPI_ClockPolarityActiveLow;
+	masterConfig.ctarConfig.cpha                          = kDSPI_ClockPhaseSecondEdge;
+	masterConfig.ctarConfig.direction                     = kDSPI_MsbFirst;
+	masterConfig.ctarConfig.pcsToSckDelayInNanoSec        = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+
+	masterConfig.whichPcs           = kDSPI_Pcs1;
+	masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
+
+	masterConfig.enableContinuousSCK        = false; //Caracteristicas del SPI, que el reloj siempre se este generando
+	masterConfig.enableRxFifoOverWrite      = false; //Tienes FIFOS; recuibe mas de un byte a la vez, "quiero que me sobrescribas el valor mas alto o no"
+	masterConfig.enableModifiedTimingFormat = false;
+	masterConfig.samplePoint                = kDSPI_SckToSin0Clock; //Nos sirve para ajustar la comunicacion
+
+	DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
+
+	/*Fin de la configuración*/
+
 }
+void Recive_Data(uint8_t x){
+	dspi_half_duplex_transfer_t TransferMemory;
 
-void Recive_Data(uint8_t x)
-{
-    dspi_half_duplex_transfer_t TransferMemory;
+	uint8_t g_master_txADDRImage_1[bit_4] = {0x03, 0x04, 0x00, 0x00};
+	uint8_t g_master_txADDRImage_2[bit_4] = {0x03, 0x04, 0x10, 0x00};
+	uint8_t g_master_txADDRImage_3[bit_4] = {0x03, 0x04, 0x20, 0x00};
+	uint8_t g_master_txADDRImage_4[bit_4] = {0x03, 0x04, 0x30, 0x00};
+	uint8_t g_master_txADDRImage_5[bit_4] = {0x03, 0x04, 0x40, 0x00};
 
-    const uint8_t g_master_txADDRImage[][4] = {{0x03, 0x04, 0x00, 0x00}, {0x03, 0x04, 0x10, 0x00}, {0x03, 0x04, 0x20, 0x00}, {0x03, 0x04, 0x30, 0x00}, {0x03, 0x04, 0x40, 0x00}};
+	switch(x){
+	case bit_1:
+		TransferMemory.txData					= g_master_txADDRImage_1;
+		TransferMemory.rxData					= g_master_rxBuffImage_1;
+		break;
 
-    uint8_t *rxBuffer = g_master_rxBuffImage[x - 1];
+	case bit_2:
+		TransferMemory.txData					= g_master_txADDRImage_2;
+		TransferMemory.rxData					= g_master_rxBuffImage_2;
+		break;
 
-    TransferMemory.txData = g_master_txADDRImage[x - 1];
-    TransferMemory.rxData = rxBuffer;
-    TransferMemory.txDataSize = 4;
-    TransferMemory.rxDataSize = IMAGE_MEMORY_SIZE;
-    TransferMemory.isTransmitFirst = true;
-    TransferMemory.isPcsAssertInTransfer = true;
-    TransferMemory.configFlags = (x == 5) ? kDSPI_MasterCtar0 | kDSPI_MasterPcs0 | kDSPI_MasterPcsContinuous : kDSPI_MasterCtar1 | kDSPI_MasterPcs1 | kDSPI_MasterPcsContinuous;
+	case bit_3:
+		TransferMemory.txData					= g_master_txADDRImage_3;
+		TransferMemory.rxData					= g_master_rxBuffImage_3;
+		break;
 
-    DSPI_MasterHalfDuplexTransferBlocking(EXAMPLE_DSPI_MASTER_BASEADDR, &TransferMemory);
+	case bit_4:
+		TransferMemory.txData					= g_master_txADDRImage_4;
+		TransferMemory.rxData					= g_master_rxBuffImage_4;
+		break;
+
+	case bit_5:
+		TransferMemory.txData					= g_master_txADDRImage_5;
+		TransferMemory.rxData					= g_master_rxBuffImage_5;
+		break;
+
+	default:
+		TransferMemory.txData					= g_master_txADDRImage_1;
+		TransferMemory.rxData					= g_master_rxBuffImage_1;
+		break;
+	}
+
+	TransferMemory.txDataSize				= bit_4;
+	TransferMemory.rxDataSize				= IMAGE_MEMORY_SIZE;
+	TransferMemory.isTransmitFirst			= true;
+	TransferMemory.isPcsAssertInTransfer	= true;
+	TransferMemory.configFlags				= kDSPI_MasterCtar1 | kDSPI_MasterPcs1 | kDSPI_MasterPcsContinuous;
+
+	DSPI_MasterHalfDuplexTransferBlocking(EXAMPLE_DSPI_MASTER_BASEADDR, &TransferMemory);
 }
 
 
